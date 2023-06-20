d8094098fb0aec161bfc3ec3e57ef39a8be1e774
mumba <61057460+mumba0@users.noreply.github.com>
2023-06-20 08:31:15 +0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+const Header = ({title, onAdd}) => {
+  return (
+    <header className='header'>
+     <h1>{title}</h1> 
+     <Button color='green' onClick=
+     {onAdd}/>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Mumba Diary',
+}
+
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+//CSS in Js
+//const headingStyle = {
+  //  color: 'steelblue', 
+    //backgroundColor: 'black'
+//}
+export default Header
